a98482299e4ca9dc73c29d09fb154d0badbc9b24
TaterTat24 <C24Benjamin.Tat@afacademy.af.edu>
2023-02-14 14:43:22 -0700

    Initial

--- listAsLinkedList.c
+++ listAsLinkedList.c
@@ -8,3 +8,19 @@
  */
 
 // Use your solution from previous labs
+#include "listAsLinkedList.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+Node* createNumberNode(double theNumberToPutInTheNode) {
+    Node* theNode = malloc(sizeof(Node));
+
+    theNode->next = NULL;
+    theNode->data = REAL_NUMBER;
+
+    theNode->data = (double*)malloc(sizeof(double));
+    *(double*)theNode->data = theNumberToPutInTheNode;
+
+    return theNode;
+}
--- queueAsLinkedList.c
+++ queueAsLinkedList.c
@@ -1,8 +1,24 @@
 /** 
  * ===========================================================
- * Name: 
- * Section: 
+ * Name: Benjamin Tat
+ * Section: T6
  * Project: Queue ADT implemented using linked list
  * Purpose: Queue ADT library - FIFO
  * ===========================================================
  */
+
+#include "queueAsLinkedList.h"
+
+QueueAsLinkedList* queueInit(){
+    QueueAsLinkedList* queue = createLinkedList();
+}
+
+void queueEnqueue(QueueAsLinkedList* queue, double element){
+    appendElementLinkedList(queue, element);
+}
+
+double queueDequeue(QueueAsLinkedList* queue){
+    double valueAtFront = getElementLinkedList(queue, 0);
+    deleteElementLinkedList(queue, 0);
+    return valueAtFront;
+}
